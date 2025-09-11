984e6fa03c8519290e5a0d1cb2599ecc313d722c
harsh kashyap <harshkashyap609@gmail.com>
2025-09-11 21:52:38 +0530

    new

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config({path:"E:\social-media-analyzer\frontend\src\services\.env"})
-const API_BASE_URL = "http://localhost:5000/api" // Ensure this matches your backend port
+// import dotenv from 'dotenv';
+// dotenv.config({path:"E:\social-media-analyzer\frontend\src\services\.env"})
+const API_BASE_URL = "https://social-media-content-analyser-vfdc.onrender.com/" // Ensure this matches your backend port
 
 export const analyzeContent = async (content) => {
     try {
